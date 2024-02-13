4944ecbc23fbd7dcdda3c95dbcf9d2b2232df78a
blue linden <dev@bluelinden.art>
2024-02-12 20:31:00 -0500

    unify tsconfigs, re-engineer storage

---
+++ serialkit/engine/src/storage.ts
@@ -0,0 +1,178 @@
+import SKBasicStorage from "../../utils/src/basic-storage";
+import type * as SKTypes from "engine/base/page.types";
+
+export class StorageCatalog {
+	#storageInstances: Map<string, SKBasicStorage> = new Map();
+	#debugMessageCallback:
+		| ((
+				message: SKTypes.SKDebugMessage,
+				messageArray: SKTypes.SKDebugMessage[]
+		  ) => void)
+		| undefined;
+	onDebugMessage(
+		callback: (
+			message: SKTypes.SKDebugMessage,
+			messageArray: SKTypes.SKDebugMessage[]
+		) => void
+	) {
+		if (import.meta.env.DEV === true) {
+			this.#debugMessageCallback = callback;
+			this.#debugMessages.forEach((message) => callback(message, []));
+		}
+	}
+	#debugMessages: SKTypes.SKDebugMessage[] = [];
+	#debugMessage(message: SKTypes.SKDebugMessage) {
+		if (!import.meta.env.DEV) return;
+		this.#debugMessages.push(message);
+		if (this.#debugMessageCallback) {
+			this.#debugMessageCallback(message, this.#debugMessages);
+		}
+	}
+
+	open(namespace: string, compression: boolean = false) {
+		this.#debugMessage({
+			type: "debug",
+			data: {
+				category: "storage",
+				isError: false,
+				message: `Opening storage for ns ${namespace}`,
+			},
+		});
+		if (!this.#storageInstances.has(namespace)) {
+			// check if namespace localstorage key has RAW in front of it
+			const value = localStorage.getItem(namespace);
+			let isAlreadyCompressed = false;
+			if (value && value.startsWith("OBF:")) {
+				isAlreadyCompressed = true;
+				this.#debugMessage({
+					type: "debug",
+					data: {
+						category: "storage",
+						isError: false,
+						message: `Storage for ns ${namespace} exists and is obfuscated`,
+					},
+				});
+			} else if (value && value.startsWith("RAW:")) {
+				this.#debugMessage({
+					type: "debug",
+					data: {
+						category: "storage",
+						isError: false,
+						message: `Storage for ns ${namespace} exists and is not obfuscated`,
+					},
+				});
+			}
+			const shouldCompress = compression;
+
+			const storage = new SKBasicStorage(namespace, shouldCompress, import.meta.env.DEV ? this.#debugMessage.bind(this) : undefined);
+
+			this.#storageInstances.set(namespace, storage);
+		}
+
+		let openedNamespace = this.#storageInstances.get(namespace)!;
+
+		if (import.meta.env.DEV) {
+			openedNamespace = new Proxy(
+				this.#storageInstances.get(namespace)!,
+				this.#debugProxyHandler()
+			);
+		}
+
+		openedNamespace.load();
+
+		return openedNamespace;
+	}
+
+	#debugProxyHandler() {
+		const thisStorageCatalog = this;
+		return {
+			get: function (
+				target: SKBasicStorage,
+				prop: keyof SKBasicStorage,
+				receiver: any
+			) {
+				if (
+					prop in target &&
+					typeof target[prop as keyof SKBasicStorage] === "function"
+				) {
+					switch (prop) {
+						case "set":
+							return function (key: string, value: any) {
+								thisStorageCatalog.#debugMessage({
+									type: "debug",
+									data: {
+										category: "storage",
+										isError: false,
+										message: `Setting key ${key} to ${value} in ns ${target.storageKey}`,
+									},
+								});
+								return target.set.apply(target, [key, value]);
+							};
+						case "get":
+							return function (key: string) {
+								// thisStorageCatalog.#debugMessage({
+								// 	type: "debug",
+								// 	data: {
+								// 		category: "storage",
+								// 		isError: false,
+								// 		message: `Getting key ${key} of ns ${target.storageKey}`,
+								// 	},
+								// });
+								const value = target.get.apply(target, [key]);
+								if (value === undefined) {
+									thisStorageCatalog.#debugMessage({
+										type: "debug",
+										data: {
+											category: "storage",
+											isError: false,
+											message: `Key ${key} not found in ns ${target.storageKey}`,
+										},
+									});
+									return undefined;
+								}
+								thisStorageCatalog.#debugMessage({
+									type: "debug",
+									data: {
+										category: "storage",
+										isError: false,
+										message: `Got value ${value} of key ${key} of ns ${target.storageKey}`,
+									},
+								});
+								return value;
+							};
+						case "load":
+							return function () {
+								thisStorageCatalog.#debugMessage({
+									type: "debug",
+									data: {
+										category: "storage",
+										isError: false,
+										message: `Loading ns ${target.storageKey} from local storage`,
+									},
+								});
+								return target.load.apply(target, []);
+							};
+						case "commit":
+							return function () {
+								thisStorageCatalog.#debugMessage({
+									type: "debug",
+									data: {
+										category: "storage",
+										isError: false,
+										message: `Manually committing ns ${target.storageKey} to local storage`,
+									},
+								});
+								return target.commit.apply(target, []);
+							};
+						default:
+							return target[prop as keyof SKBasicStorage];
+					}
+				} else {
+					return target[prop as keyof SKBasicStorage];
+				}
+			},
+		};
+	}
+}
+
+export default new StorageCatalog();
---
+++ serialkit/utils/src/basic-storage.ts
@@ -0,0 +1,211 @@
+import { compress, decompress } from "compress-json";
+import type * as SKTypes from "engine/base/page.types";
+
+export default class SKStorage {
+	#values: Map<string, any> = new Map();
+	nextCommitTime: number = -1;
+
+	constructor(
+		public readonly storageKey = "gamedata",
+		private enableCompression = false,
+		debugCallback?: (message: SKTypes.SKDebugMessage) => void
+	) {
+		if (debugCallback) {
+			this.#debugCallback = debugCallback;
+		}
+		this.#debugMessage({
+			type: "debug",
+			data: {
+				category: "storage",
+				isError: false,
+				message: `Constructing LocalStorage handler for ns ${storageKey}`,
+				isInternal: true,
+			},
+		});
+	}
+
+	#debugCallback?: (message: SKTypes.SKDebugMessage) => void;
+
+	#debugMessage(message: SKTypes.SKDebugMessage) {
+		if (this.#debugCallback) {
+			this.#debugCallback(message);
+		}
+	}
+
+	set(key: string, value: any) {
+		this.#values.set(key, value);
+		// disable autocommit
+		// this.nextCommitTime = Date.now() + 499;
+		// setTimeout(() => this.checkIfShouldCommit(), 500);
+	}
+
+	// check if should commit by seeing if the last commit time is less than 500ms ago. if it is, exit. if it isn't, commit.
+	checkIfShouldCommit() {
+		this.#debugMessage({
+			type: "debug",
+			data: {
+				category: "storage",
+				isError: false,
+				message: `Checking if should commit LocalStorage for ns ${this.storageKey}`,
+				isInternal: true,
+			},
+		});
+		if (
+			this.nextCommitTime < Date.now() - 450 &&
+			this.nextCommitTime !== -1
+		) {
+			this.#debugMessage({
+				type: "debug",
+				data: {
+					category: "storage",
+					isError: false,
+					message: `Should commit LocalStorage for ns ${this.storageKey}`,
+					isInternal: true,
+				},
+			});
+			this.commit();
+		}
+	}
+
+	get(key: string) {
+		return this.#values.get(key);
+	}
+
+	commit() {
+		let final: string;
+
+		if (this.enableCompression) {
+			// look - this isn't intended to be secure. not even that hard to break. but i want people to have a consistent experience, and to not f*ck around with games in this engine.
+			const valuesasObject: Record<string, any> = {};
+			this.#values.forEach((value, key) => {
+				valuesasObject[key] = value;
+			});
+			const compressed = compress(valuesasObject);
+			const stringified = JSON.stringify(compressed);
+			const encoded = encodeURIComponent(stringified);
+			const base64 = btoa(encoded);
+
+			// generate hash
+			const hashPre = crypto.subtle.digest(
+				"SHA-256",
+				new TextEncoder().encode(stringified)
+			);
+
+			hashPre.then((hash) => {
+				final =
+					"OBF:" +
+					btoa(
+						encodeURIComponent(
+							JSON.stringify(
+								compress({
+									hh: Array.from(new Uint8Array(hash))
+										.map((b) => b.toString(16).padStart(2, "0"))
+										.join(""),
+									ve: base64,
+								})
+							)
+						)
+					);
+
+				this.#debugMessage({
+					type: "debug",
+					data: {
+						category: "storage",
+						isError: false,
+						message: `Committing LocalStorage for ns ${this.storageKey}`,
+						isInternal: true,
+					},
+				});
+				localStorage.setItem(this.storageKey, final);
+			});
+		} else {
+			final = JSON.stringify(this.#values);
+			localStorage.setItem(this.storageKey, "RAW:" + final);
+		}
+	}
+
+	load() {
+		this.#debugMessage({
+			type: "debug",
+			data: {
+				category: "storage",
+				isError: false,
+				message: `Loading LocalStorage for ns ${this.storageKey}`,
+				isInternal: true,
+			},
+		});
+		try {
+		const value = localStorage.getItem(this.storageKey);
+		if (value) {
+			const [type, data] = value.split(":");
+			if (type === "OBF") {
+				// decode the stored data
+				const decoded = decodeURIComponent(atob(data));
+				// decompress the data
+				const decompressed = decompress(JSON.parse(decoded));
+				// extract the hash value
+				const hashValue = decompressed.hh;
+				// decode the base64 encoded data
+				const base64Decoded = atob(decompressed.ve);
+				// decode the URI component
+				const decodedURI = decodeURIComponent(base64Decoded);
+				// calculate the hash
+				const hash = crypto.subtle.digest(
+					"SHA-256",
+					new TextEncoder().encode(decodedURI)
+				);
+				// compare the calculated hash with the hash from stored data
+				hash.then((calculatedHash) => {
+					const calculatedHashStr = Array.from(
+						new Uint8Array(calculatedHash)
+					)
+						.map((b) => b.toString(16).padStart(2, "0"))
+						.join("");
+					if (calculatedHashStr === hashValue) {
+						this.#debugMessage({
+							type: "debug",
+							data: {
+								category: "storage",
+								isError: false,
+								message: `Hash valid for LocalStorage ns ${this.storageKey}`,
+								isInternal: true,
+							}
+						})
+						const valuesAsCompressedObject = JSON.parse(decodedURI);
+						const valuesAsObject = decompress(valuesAsCompressedObject);
+
+						// zero fucking clue what i'm doing here
+						delete valuesAsObject["0"];
+						delete valuesAsObject["1"];
+						this.#values = new Map(Object.entries(valuesAsObject));
+						this.#debugMessage({
+							type: "debug",
+							data: {
+								category: "storage",
+								isError: false,
+								message: `Loaded LocalStorage for ns ${
+									this.storageKey
+								}, with values ${JSON.stringify(valuesAsObject)}`,
+								isInternal: true,
+							},
+						});
+					} else {
+						this.#debugMessage({
+							type: "debug",
+							data: {
+								category: "storage",
+								isError: true,
+								message: `Hash invalid for LocalStorage ns ${this.storageKey}`,
+								isInternal: true,
+							},
+						});
+					}
+				});
+			} else if (type === "RAW") {
+				this.#values = JSON.parse(data);
+			}
+		}} catch (e) {
+			
+		}
+	}
+}
